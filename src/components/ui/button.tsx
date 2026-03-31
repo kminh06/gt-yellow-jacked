44fbc032908ecd7ffd45c9dba1718fcf7a212d9c
Julie Wang <juliewang9171@gatech.edu>
2026-03-31 19:21:35 -0400

    2

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground font-bold border-1 border-secondary shadow-lg hover:bg-primary/90",
+          "bg-primary text-primary-foreground font-bold border-secondary shadow-lg hover:bg-primary/90",
         destructive:
           "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
@@ -23,7 +23,7 @@ const buttonVariants = cva(
           "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-9 rounded-full px-6 py-2 has-[>svg]:px-3",
+      default: "h-12 rounded-full px-10 py-5 has-[>svg]:px-3",
         xs: "h-6 rounded-md gap-1 px-2 text-xs has-[>svg]:px-1.5 [&_svg:not([class*='size-'])]:size-3",
         sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
         lg: "h-10 rounded-full px-6 has-[>svg]:px-4",
